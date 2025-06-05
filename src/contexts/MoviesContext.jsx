eae6c226cd305ec0d1f77452f442abb042eb3c9d
petraalpigiano <petraalpigiano@gmail.com>
2025-06-05 13:16:21 +0200

    add static call

--- src/contexts/MoviesContext.jsx
+++ src/contexts/MoviesContext.jsx
@@ -1,8 +1,18 @@
 import axios from "axios";
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect } from "react";
 const MoviesContext = createContext();
 
 function MoviesProvider({ children }) {
+  const apiURL = "https://api.themoviedb.org/3";
+  const apiKey = import.meta.env.VITE_API_KEY;
+
+  useEffect(() => {
+    axios
+      .get(`${apiURL}/search/movie?query=Hurry&api_key=${apiKey}`)
+      .then((res) => {
+        console.log(res.data.results);
+      });
+  }, []);
   return (
     <>
       <MoviesContext.Provider>{children}</MoviesContext.Provider>
