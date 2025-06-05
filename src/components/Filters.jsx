ee53945655b520bcaf3d749d19768a024bd21323
petraalpigiano <petraalpigiano@gmail.com>
2025-06-05 18:15:58 +0200

    add header components

---
+++ src/components/Filters.jsx
@@ -0,0 +1,10 @@
+import SearchBar from "./SearchBar";
+import FilterSelect from "./FilterSelect";
+export default function Filters() {
+  return (
+    <>
+      <SearchBar />
+      <FilterSelect />
+    </>
+  );
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,8 @@
-import { useState } from "react";
-import { useMovies } from "../contexts/MoviesContext";
-
+import Filters from "./Filters";
 export default function Header() {
-  const { search } = useMovies();
-  const [input, setInput] = useState("");
-  function handleFormSubmit(e) {
-    e.preventDefault();
-    search(input);
-  }
-
   return (
     <>
-      <nav className="navbar bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand">Boolflix</a>
-          <form onSubmit={handleFormSubmit} className="d-flex" role="search">
-            <input
-              value={input}
-              onChange={(e) => {
-                setInput(e.target.value);
-              }}
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
-        </div>
-      </nav>
+      <Filters />
     </>
   );
 }
